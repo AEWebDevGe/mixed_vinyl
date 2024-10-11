dae954a44f6ebebd2d0f61b7de9138f94c5b32e2
Nicolas Garbini <webdev@audioeffetti.it>
2024-10-11 17:18:11 +0200

    ToDo List aggiunta

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,16 +3,29 @@
 namespace App\Controller;
 
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController {
+class VinylController extends AbstractController {
   #[Route('/')]
   //Funzione che reindirizza il contenuto della homepage
   public function homepage() : Response {
-    return new Response('Title: Ajeje Brazorf');
+    $tracks = [
+        ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+        ['song' => 'Waterfalls', 'artist' => 'TLC'],
+        ['song' => 'Creep', 'artist' => 'Radiohead'],
+        ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+        ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+        ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ['song' => 'Paradise City', 'artist' => 'Guns N Roses'],
+    ];
+    return $this->render('vinyl/homepage.html.twig', [
+        'title' => 'PB & James',
+        'tracks' => $tracks,
+    ]);
+    //return new Response('Title: Ajeje Brazorf');
   }
 
   #[Route('/browse/{slug}')]
